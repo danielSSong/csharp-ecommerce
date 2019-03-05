948201fbf4b53d514fc56ea8e931db51a59ca623
danielSSong <danielzzang5517@gmail.com>
2019-03-05 13:49:51 +1100

    Added Basket models

--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -17,10 +17,10 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
         //Enable-Migrations
-
-        //Add-Migration Initial
-
+        //Add-Migration AddBasket
         //Update-Database
     }
 }
